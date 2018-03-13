880d5ba683c1babcffaabf8f89efaaf680d605a0
heylexy <37335410+heylexy@users.noreply.github.com>
2018-03-13 21:00:16 +0800

    Update LumberjackRacerForm.cs

--- LumberjackRacerForm.cs
+++ LumberjackRacerForm.cs
@@ -62,7 +62,7 @@ namespace LumberRacer
 
                 var textbox = new NumericUpDown
                 {
-                    Maximum = displayAttribute?.Max ?? 400,
+                    Maximum = displayAttribute?.Max ?? 800,
                     Value = displayAttribute?.DefaultValue ?? 0
                 };
 
